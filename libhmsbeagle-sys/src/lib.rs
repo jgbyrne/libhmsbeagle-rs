8e6b4ba5aa1de0c6e276ff2aad5544c68e11a37e
Jack Byrne <jgbyrne9@gmail.com>
2021-11-04 04:57:21 +0000

    wrapping some beagle fns and rustifying types

--- libhmsbeagle-sys/src/lib.rs
+++ libhmsbeagle-sys/src/lib.rs
@@ -6,41 +6,45 @@
 // This matches bindgen::Builder output
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
 
-#[cfg(test)]
-mod tests {
-    use crate::*;
-    #[test]
-    fn basic_test() {
-        unsafe {
-            let resourceList = std::ptr::null_mut();
+use std::ffi::{CStr,CString};
 
-            let mut returnInfo = BeagleInstanceDetails {
-                resourceNumber: 0,
-                resourceName: std::ptr::null_mut(),
-                implName: std::ptr::null_mut(),
-                implDescription: std::ptr::null_mut(),
-                flags: 0,
-            };
+mod types;
+pub use crate::types::*;
 
-            // create an instance of the BEAGLE library
-            let instance = beagleCreateInstance(
-                        3,             /* Number of tip data elements (input) */
-                        2,             /* Number of partials buffers to create (input) -- internal node count */
-                        3,             /* Number of compact state representation buffers to create -- for use with setTipStates (input) */
-                        4,             /* Number of states in the continuous-time Markov chain (input) -- DNA */
-                        16,            /* Number of site patterns to be handled by the instance (input) -- not compressed in this case */
-                        1,             /* Number of eigen-decomposition buffers to allocate (input) */
-                        4,             /* Number of transition matrix buffers (input) -- one per edge */
-                        1,             /* Number of rate categories */
-                        0,             /* Number of scaling buffers -- can be zero if scaling is not needed*/
-                        resourceList,  /* List of potential resource on which this instance is allowed (input, NULL implies no restriction */
-                        0,             /* Length of resourceList list (input) -- not needed to use the default hardware config */
-                        0,             /* Bit-flags indicating preferred implementation charactertistics, see BeagleFlags (input) */
-                        0,             /* Bit-flags indicating required implementation characteristics, see BeagleFlags (input) */
-                        &mut returnInfo
-                        );
+pub fn create_instance(tipCount: i32, partialsBufferCount: i32,
+                       compactBufferCount: i32, stateCount: i32,
+                       patternCount: i32, eigenBufferCount: i32,
+                       matrixBufferCount: i32, categoryCount: i32,
+                       scaleBufferCount: i32,
+                       // Let's ignore resource restrictions for now...
+                       preferenceFlags: Flags,
+                       requirementFlags: Flags) -> InstanceDetails {
+    unsafe {
+        let mut returnInfo = BeagleInstanceDetails {
+            resourceNumber: 0,
+            resourceName: std::ptr::null_mut(),
+            implName: std::ptr::null_mut(),
+            implDescription: std::ptr::null_mut(),
+            flags: 0,
+        };
 
-            assert_eq!(returnInfo.resourceNumber, 0);
+        // create an instance of the BEAGLE library
+        let instance = beagleCreateInstance(
+            tipCount, partialsBufferCount, compactBufferCount, stateCount, patternCount, eigenBufferCount,
+            matrixBufferCount, categoryCount, scaleBufferCount, std::ptr::null_mut(), 0,
+            preferenceFlags.bits(), requirementFlags.bits(), &mut returnInfo);
+
+        InstanceDetails {
+            resourceNumber: returnInfo.resourceNumber,
+            resourceName: CStr::from_ptr(returnInfo.resourceName).to_str().unwrap().to_owned(), 
+            implName: CStr::from_ptr(returnInfo.implName).to_str().unwrap().to_owned(), 
+            implDescription: CStr::from_ptr(returnInfo.implDescription).to_str().unwrap().to_owned(), 
+            flags: Flags::from_bits(returnInfo.flags).unwrap(),
         }
     }
+}
+
+#[cfg(test)]
+mod tests {
+
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,10 @@
+extern crate beagle;
+
 #[cfg(test)]
 mod tests {
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn new_instance_test() {
+        let details = beagle::create_instance(3, 2, 3, 4, 16, 1, 4, 1, 0, beagle::Flags::empty(), beagle::Flags::empty());
+        println!("{:?}", details);
     }
 }
