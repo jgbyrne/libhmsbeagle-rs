b7e36c1f1c1b5f29b6eaab01b55883f1f6f797a6
Jack Byrne <jack.byrne@durham.ac.uk>
2022-01-07 19:11:35 +0000

    Working towards mcmc :: alternative buffers

--- src/types.rs
+++ src/types.rs
@@ -13,6 +13,17 @@ pub struct Model {
 }
 
 #[derive(Debug)]
+pub struct Alternates {
+    // number of buffers before alternates
+    n_partials_core: i32,
+    n_matrices_core: i32,
+    n_scalers_core: i32,
+
+    alt_partials: Vec<bool>, // len n_internals
+    alt_matrices: Vec<bool>, // len n_nodes - 1
+}
+
+#[derive(Debug)]
 pub struct Instance {
     id: i32,
     details: sys::InstanceDetails,
@@ -22,22 +33,37 @@ pub struct Instance {
     n_rates: i32,
     n_nodes: i32,
     n_tips: i32,
+
+    // number of buffers including alternates
+    n_partials: i32,
+    n_matrices: i32,
+    n_scalers: i32,
+
+    models: Vec<Model>,
+
     tip_partials: bool,
     scaling: bool,
-    models: Vec<Model>,
+    alternates: Option<Alternates>,
 }
 
 impl Instance {
-    pub fn new(n_states: i32, n_sites: i32, n_rates: i32, n_nodes: i32,
-               n_tips: i32, tip_partials: bool, scaling: bool, models: Vec<Model>) -> Instance {
+    pub fn new(n_states: i32, n_sites: i32, n_rates: i32, n_nodes: i32, n_tips: i32,
+               tip_partials: bool, scaling: bool, alternates: bool, models: Vec<Model>) -> Instance {
 
         let n_internals = n_nodes - n_tips;
 
-        let n_partials = if tip_partials { n_nodes } else { n_internals };
+        let n_partials_core = if tip_partials { n_nodes } else { n_internals };
+        let n_partials = if alternates { n_partials_core + n_internals } else { n_partials_core };
         let n_seqs = if tip_partials { 0 } else { n_tips };
 
+        let n_matrices_core = n_nodes - 1;
+        let n_matrices = if alternates { n_matrices_core * 2 } else { n_matrices_core };
+
         // A buffer for each internal node and a cumulative buffer 
-        let n_scalers =  if scaling { n_internals + 1 } else { 0 };
+        let n_scalers_core =  if scaling { n_internals + 1 } else { 0 };
+        let n_scalers = if scaling {
+            if alternates { n_scalers_core + n_internals } else { n_scalers_core }
+        } else { 0 };
 
         let prefs = sys::Flags::SCALING_MANUAL | sys::Flags::SCALERS_LOG;
 
@@ -47,7 +73,7 @@ impl Instance {
                                                  n_states,
                                                  n_sites,
                                                  models.len() as i32,
-                                                 n_nodes - 1,
+                                                 n_matrices,
                                                  n_rates,
                                                  n_scalers,
                                                  None,
@@ -65,6 +91,20 @@ impl Instance {
 
         sys::set_pattern_weights(id, &(0..n_sites).map(|_| 1.0).collect::<Vec<f64>>()[..]);
 
+        let alts = match alternates {
+            false => None,
+            true => {
+                Some(Alternates {
+                    n_partials_core,
+                    n_matrices_core,
+                    n_scalers_core,
+
+                    alt_partials: (0..n_internals).map(|_| false).collect::<Vec<bool>>(),
+                    alt_matrices: (0..(n_nodes - 1)).map(|_| false).collect::<Vec<bool>>(),
+                })
+            },
+        };
+
         Instance {
             id,
             details,
@@ -74,25 +114,113 @@ impl Instance {
             n_rates,
             n_nodes,
             n_tips,
-            tip_partials,
+
+            n_partials,
+            n_matrices,
+            n_scalers,
+
+            models,
+
+            alternates: alts,
+
             scaling,
-            models
+            tip_partials,
         }
     }
 
-    pub fn scaling_buffer(&self, beagle_internal_id: i32) -> i32 {
-        if self.scaling {
-            if beagle_internal_id < self.n_tips {
-                panic!("Tips have no scaling buffers")
+    pub fn build_operation(&self, parent_id: i32, left_id: i32, right_id: i32) -> sys::Operation {
+        sys::Operation {
+            destinationPartials: self.partials_buffer(parent_id),
+            destinationScaleWrite: self.scaling_buffer(parent_id),
+            destinationScaleRead: sys::OpCodes::OP_NONE as i32,
+            child1Partials: self.partials_buffer(left_id),
+            child1TransitionMatrix: self.matrix_buffer(left_id),
+            child2Partials: self.partials_buffer(right_id),
+            child2TransitionMatrix: self.matrix_buffer(right_id),
+        }
+    }
+
+    pub fn partials_buffer(&self, node_id: i32) -> i32 {
+        assert!(node_id < self.n_nodes);
+        if let Some(alt) = &self.alternates {
+            if (node_id >= self.n_tips) {
+                let internal_offset = node_id - self.n_tips;
+                if alt.alt_partials[internal_offset as usize] {
+                    // |<     n_partials_core    >|
+                    // [ n_tips ] [ n_internals ] | [ n_internals ]
+                    return alt.n_partials_core + internal_offset;
+                }
             }
-            beagle_internal_id - self.n_tips + 1
+        }
+        node_id
+    }
+
+    pub fn matrix_buffer(&self, node_id: i32) -> i32 {
+        assert!(node_id < self.n_nodes - 1);
+        if let Some(alt) = &self.alternates {
+            if alt.alt_matrices[node_id as usize] {
+                // n_matrices_core = n_nodes - 1
+                return alt.n_matrices_core + node_id;
+            }
+        }
+        node_id
+    }
+
+    pub fn scaling_buffer(&self, internal_id: i32) -> i32 {
+        assert!(internal_id >= self.n_tips);
+        assert!(internal_id < self.n_nodes);
+        if self.scaling {
+            let internal_offset = internal_id - self.n_tips;
+            if let Some(alt) = &self.alternates {
+                if alt.alt_partials[internal_offset as usize] {
+                    // |<  n_scalers_core >|
+                    // [1] [ n_internals ] | [ n_internals ]
+                    return alt.n_scalers_core + internal_offset;
+                }
+            }
+            1 + internal_offset 
         }
         else {
             sys::OpCodes::OP_NONE as i32
         }
     }
 
+    pub fn is_alt_partials(&self, node_id: i32) -> bool {
+        assert!(node_id < self.n_nodes);
+        if node_id >= self.n_tips {
+            let alts = self.alternates.as_ref().expect("No alternates");
+            alts.alt_partials[(node_id - self.n_tips) as usize]
+        }
+        else {
+            false
+        }
+    }
+
+    pub fn is_alt_matrix(&self, node_id: i32) -> bool {
+        assert!(node_id < self.n_nodes - 1);
+        let alts = self.alternates.as_ref().expect("No alternates");
+        alts.alt_matrices[node_id as usize]
+    }
+
+    pub fn flip_alt_partials(&mut self, node_id: i32) {
+        assert!(node_id < self.n_nodes);
+        if node_id >= self.n_tips {
+            let alts = self.alternates.as_mut().expect("No alternates");
+            let idx = (node_id - self.n_tips) as usize; 
+            alts.alt_partials[idx] = !alts.alt_partials[idx];
+        }
+    }
+
+    pub fn flip_alt_matrix(&mut self, node_id: i32) {
+        assert!(node_id < self.n_nodes - 1);
+        let alts = self.alternates.as_mut().expect("No alternates");
+        let idx = node_id as usize;
+        alts.alt_matrices[idx] = !alts.alt_matrices[idx];
+    }
+
+
     pub fn update_matrices(&mut self, model_id: i32, edge_lengths: Vec<f64>) {
+        assert!(edge_lengths.len() == (self.n_nodes - 1) as usize);
         let matrix_idxs = &(0..(self.n_nodes - 1)).map(|i| i as i32).collect::<Vec<i32>>()[..];
         sys::update_transition_matrices(self.id, model_id, matrix_idxs, None, None, &edge_lengths);
     }
@@ -116,7 +244,7 @@ impl Instance {
     }
 
     pub fn perform_operations(&mut self, ops: Vec<sys::Operation>) {
-        sys::update_partials(self.id, ops, if self.scaling { 0 } else { -1 });
+        sys::update_partials(self.id, ops, -1);
     }
     
     pub fn wait_for_partials(&mut self, dests: Vec<i32>) {
@@ -129,7 +257,11 @@ impl Instance {
             true => {
                 let cumulative_scale_idx = 0;
                 let n_internals = self.n_nodes - self.n_tips;
-                let internal_scalers = (1..=n_internals).collect::<Vec<i32>>();
+                let mut internal_scalers = vec![];
+                for i in 0..n_internals {
+                    internal_scalers.push(self.scaling_buffer(self.n_tips + i));
+                }
+
                 sys::reset_scale_factors(self.id, cumulative_scale_idx);
                 sys::accumulate_scale_factors(self.id, &internal_scalers, cumulative_scale_idx);
                 cumulative_scale_idx 
@@ -137,7 +269,7 @@ impl Instance {
         };
 
         let mut outSumLogLikelihood: f64 = 666.0;
-        sys::calculate_root_log_likelihoods(self.id, &[root_id], &[model_id], &[model_id], &[cumulative_scale_idx], &mut outSumLogLikelihood);
+        sys::calculate_root_log_likelihoods(self.id, &[self.partials_buffer(root_id)], &[model_id], &[model_id], &[cumulative_scale_idx], &mut outSumLogLikelihood);
         outSumLogLikelihood
     }
 
